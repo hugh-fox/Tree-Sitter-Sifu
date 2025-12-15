c0140e6c6c03acb4695de7c1a03073dd781c0c6c
hugh-fox <checkmateguy@gmail.com>
2025-12-15 08:21:27 -0700

    rely on precedence instead of manually configuring operand sets

--- tree-sitter-sifu/grammar.js
+++ tree-sitter-sifu/grammar.js
@@ -19,133 +19,63 @@ module.exports = grammar({
   ],
 
   rules: {
+
     pattern: ($) => optional($._op),
 
-    comment: ($) => token(
-      seq(
-        "#",
-        /.*/
-      )
-    ),
-
-    semicolon: ($) => prec.right(1, seq(
-      field("lhs", optional($._non_semicolon)),
-      ";",
-      field("rhs", optional(choice(
-        $.semicolon,
-        $.long_match,
-        $.long_arrow,
-        $.comma,
-        $.infix,
-        $.match,
-        $.arrow,
-        $.apps,
-      ))),
-    )),
-
-    long_match: ($) => prec.right(2, seq(
-      field("lhs", optional($._non_long)),
-      "::",
-      field("rhs", optional(choice(
-        $.long_match,
-        $.long_arrow,
-        $.comma,
-        $.infix,
-        $.match,
-        $.arrow,
-        $.apps,
-      ))),
-    )),
-    long_arrow: ($) => prec.right(2, seq(
-      field("lhs", optional($._non_long)),
-      "-->",
-      field("rhs", optional(choice(
-        $.long_match,
-        $.long_arrow,
-        $.comma,
-        $.infix,
-        $.match,
-        $.arrow,
-        $.apps,
-      ))),
-    )),
-
-    comma: ($) => prec.right(3, seq(
-      field("lhs", optional($._non_comma)),
-      ",",
-      field("rhs", optional(choice(
-        $.comma,
-        $.infix,
-        $.match,
-        $.arrow,
-        $.apps,
-      ))),
-    )),
-
-    infix: ($) => prec.right(4, seq(
-      field("lhs", optional($._non_infix)),
-      field("op", $.symbol),
-      field("rhs", optional(choice(
-        $.infix,
-        $.match,
-        $.arrow,
-        $.apps,
-      ))),
-    )),
-
-    match: ($) => prec.right(5, seq(
-      field("lhs", optional($._non_short)),
-      ":",
-      field("rhs", optional(choice(
-        $.match,
-        $.arrow,
-        $.apps,
-      ))),
-    )),
-    arrow: ($) => prec.right(5, seq(
-      field("lhs", optional($._non_short)),
-      "->",
-      field("rhs", optional(choice(
-        $.match,
-        $.arrow,
-        $.apps,
-      ))),
-    )),
-
-    _non_semicolon: ($) => choice(
+    _op: ($) => choice(
+      $.semicolon,
       $.long_match,
       $.long_arrow,
       $.comma,
       $.infix,
       $.match,
       $.arrow,
-      $.apps
+      $._terms
     ),
 
-    _non_long: ($) => choice(
-      $.comma,
-      $.infix,
-      $.match,
-      $.arrow,
-      $.apps
-    ),
+    semicolon: ($) => prec.right(1, seq(
+      field("lhs", optional($._op)),
+      ";",
+      field("rhs", optional($._op)),
+    )),
 
-    _non_comma: ($) => choice(
-      $.infix,
-      $.match,
-      $.arrow,
-      $.apps
-    ),
+    long_match: ($) => prec.right(2, seq(
+      field("lhs", optional($._op)),
+      "::",
+      field("rhs", optional($._op)),
+    )),
 
-    _non_infix: ($) => choice(
-      $.match,
-      $.arrow,
-      $.apps
-    ),
+    long_arrow: ($) => prec.right(2, seq(
+      field("lhs", optional($._op)),
+      "-->",
+      field("rhs", optional($._op)),
+    )),
 
-    _non_short: ($) => $.apps,
+    comma: ($) => prec.right(3, seq(
+      field("lhs", optional($._op)),
+      ",",
+      field("rhs", optional($._op)),
+    )),
 
-    apps: ($) => prec.right(8, repeat1(
+    infix: ($) => prec.right(4, seq(
+      field("lhs", optional($._op)),
+      field("op", $.symbol),
+      field("rhs", optional($._op)),
+    )),
+
+    match: ($) => prec.right(5, seq(
+      field("lhs", optional($._op)),
+      ":",
+      field("rhs", optional($._op)),
+    )),
+
+    arrow: ($) => prec.right(5, seq(
+      field("lhs", optional($._op)),
+      "->",
+      field("rhs", optional($._op)),
+    )),
+
+    _terms: ($) => prec.right(6, repeat1(
       $._term
     )),
 
@@ -159,31 +89,13 @@ module.exports = grammar({
       $.quote,
     ),
 
-    // Unicode-aware identifiers
-    key: ($) => /\p{Lu}[\p{L}\p{N}_-]*/u,
-    variable: ($) => /\p{Ll}[\p{L}\p{N}_-]*/u,
-    number: ($) => /[0-9]+(\.[0-9]+)?/,
-    string: ($) => /"([^"\\]|\\.)*"/,
-    // Unicode-aware symbols (excluding reserved characters)
-    symbol: ($) => /[:!@$%^&*+-=|<>?\/\\~`\p{S}]+/u,
-
-    _op: ($) => choice(
-      $.semicolon,
-      $.long_match,
-      $.long_arrow,
-      $.comma,
-      $.infix,
-      $.match,
-      $.arrow,
-      $.apps
-    ),
-
     // Nested structures
     nested_pattern: ($) => prec.right(6, seq(
       "(",
       optional($._op),
       ")"
     )),
+
     nested_trie: ($) => prec.right(6, seq(
       "{",
       optional($._op),
@@ -195,5 +107,20 @@ module.exports = grammar({
       optional($._op),
       "`"
     )),
+
+    // Unicode-aware identifiers
+    key: ($) => /\p{Lu}[\p{L}\p{N}_-]*/u,
+    variable: ($) => /\p{Ll}[\p{L}\p{N}_-]*/u,
+    number: ($) => /[0-9]+(\.[0-9]+)?/,
+    string: ($) => /"([^"\\]|\\.)*"/,
+    // Unicode-aware symbols (excluding reserved characters)
+    symbol: ($) => /[:!@$%^&*+-=|<>?\/\\~`\p{S}]+/u,
+
+    comment: ($) => token(
+      seq(
+        "#",
+        /.*/
+      )
+    ),
   },
 });
