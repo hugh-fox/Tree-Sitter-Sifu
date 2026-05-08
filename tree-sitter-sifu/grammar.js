31583e38013fdfda3432c4be15413ee5aabd7b23
hugh-fox <hugh.s.fox@gmail.com>
2026-05-08 12:08:00 -0700

    Fix precedence for ops with empty tail

--- tree-sitter-sifu/grammar.js
+++ tree-sitter-sifu/grammar.js
@@ -5,6 +5,14 @@
  *
  * Parses an AST that is converted to a single Pattern. The conversion
  * algorithm assumes that all anonymous nodes have exactly one child.
+ *
+ * Operator precedence (lowest to highest):
+ *   1. semicolon  ;
+ *   2. long_match ::  /  long_arrow -->
+ *   3. comma      ,
+ *   4. infix      <symbol>
+ *   5. match      :   /  arrow      ->
+ *   6. terms      (juxtaposition)
  */
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
@@ -14,70 +22,91 @@ module.exports = grammar({
 
   extras: ($) => [
     /[\s]/,
-    // /[ \t\r]/, // Whitespace except newlines
     $.comment,
   ],
 
   rules: {
 
-    pattern: ($) => optional($._op),
+    pattern: ($) => optional($._prec1),
 
-    _op: ($) => choice(
+    // Prec 1: semicolon
+    _prec1: ($) => choice(
       $.semicolon,
-      $.long_match,
-      $.long_arrow,
-      $.comma,
-      $.infix,
-      $.match,
-      $.arrow,
-      $.terms
+      $._prec2,
     ),
 
     semicolon: ($) => prec.right(1, seq(
-      field("lhs", optional($._op)),
+      field("lhs", optional($._prec1)),
       ";",
-      field("rhs", optional($._op)),
+      field("rhs", optional($._prec2)),
     )),
 
+    // Prec 2: long_match, long_arrow
+    _prec2: ($) => choice(
+      $.long_match,
+      $.long_arrow,
+      $._prec3,
+    ),
+
     long_match: ($) => prec.right(2, seq(
-      field("lhs", optional($._op)),
+      field("lhs", optional($._prec2)),
       "::",
-      field("rhs", optional($._op)),
+      field("rhs", optional($._prec3)),
     )),
 
     long_arrow: ($) => prec.right(2, seq(
-      field("lhs", optional($._op)),
+      field("lhs", optional($._prec2)),
       "-->",
-      field("rhs", optional($._op)),
+      field("rhs", optional($._prec3)),
     )),
+
+    // Prec 3: comma
+    _prec3: ($) => choice(
+      $.comma,
+      $._prec4,
+    ),
 
     comma: ($) => prec.right(3, seq(
-      field("lhs", optional($._op)),
+      field("lhs", optional($._prec3)),
       ",",
-      field("rhs", optional($._op)),
+      field("rhs", optional($._prec4)),
     )),
+
+    // Prec 4: infix (user-defined symbol)
+    _prec4: ($) => choice(
+      $.infix,
+      $._prec5,
+    ),
 
     infix: ($) => prec.right(4, seq(
-      field("lhs", optional($._op)),
+      field("lhs", optional($._prec4)),
       field("op", $.symbol),
-      field("rhs", optional($._op)),
+      field("rhs", optional($._prec5)),
     )),
 
+    // Prec 5: match, arrow
+    _prec5: ($) => choice(
+      $.match,
+      $.arrow,
+      $._prec6,
+    ),
+
     match: ($) => prec.right(5, seq(
-      field("lhs", optional($._op)),
+      field("lhs", optional($._prec5)),
       ":",
-      field("rhs", optional($._op)),
+      field("rhs", optional($._prec6)),
     )),
 
     arrow: ($) => prec.right(5, seq(
-      field("lhs", optional($._op)),
+      field("lhs", optional($._prec5)),
       "->",
-      field("rhs", optional($._op)),
+      field("rhs", optional($._prec6)),
     )),
 
-    terms: ($) => prec.right(6, repeat1(
-      $._term
-    )),
+    // Prec 6: juxtaposition (terms)
+    _prec6: ($) => $.terms,
+
+    terms: ($) => prec.right(6, repeat1($._term)),
 
     _term: ($) => choice(
       $.key,
@@ -90,39 +119,34 @@ module.exports = grammar({
       $.quote,
     ),
 
-    // Nested structures
-    nested_pattern: ($) => prec.right(6, seq(
+    nested_pattern: ($) => seq(
       "(",
-      field("inner", optional($._op)),
+      field("inner", optional($._prec1)),
       ")"
-    )),
+    ),
 
-    nested_trie: ($) => prec.right(6, seq(
+    nested_trie: ($) => seq(
       "{",
-      field("inner", optional($._op)),
+      field("inner", optional($._prec1)),
       "}"
-    )),
+    ),
 
     quote: ($) => prec.right(5, seq(
       "`",
-      optional($._op),
+      field("inner", optional($._prec1)),
       "`"
     )),
 
-    // Unicode-aware identifiers
+    // Terminals
     key: ($) => /\p{Lu}[\p{L}\p{N}_-]*/u,
     variable: ($) => /\p{Ll}[\p{L}\p{N}_-]*/u,
     var_pattern: ($) => /\*\p{Ll}[\p{L}\p{N}_-]*/u,
     number: ($) => /[0-9]+(\.[0-9]+)?/,
     string: ($) => /"([^"\\]|\\.)*"/,
-    // Unicode-aware symbols (excluding reserved characters)
-    symbol: ($) => /[:!@$%^&*+-=|<>?\/\\~`\p{S}]+/u,
 
-    comment: ($) => token(
-      seq(
-        "#",
-        /.*/
-      )
-    ),
+    // Negative token precedence ensures reserved literals always win
+    symbol: ($) => token(prec(-1, /[:!@$%^&*+\-=|<>?\/\\~`\p{S}]+/u)),
+
+    comment: ($) => token(seq("#", /.*/)),
   },
 });
